e1d717a6c83947dd2155cb29db577c45e84848e7
Nate Korous <natekorous@workfront.com>
2018-09-17 17:28:55 -0600

    got the basic scaffold up and working. Need to implelemtn hot reload

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import './app.css'
+
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <p>
+                    This is now updated to something NEWWWWWW!!!
+                </p>
+            </div>
+        )
+    }
+}
+
+export default App
